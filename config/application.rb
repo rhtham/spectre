9eeb015b7ca2c94be02c2c12193c2ebfa8ec7f22
Rick Tham <rtham@onenorth.com>
2019-08-23 09:47:28 -0500

    moving to prod file

--- config/application.rb
+++ config/application.rb
@@ -2,8 +2,6 @@ require_relative 'boot'
 
 require 'rails/all'
 
-config.middleware.use Rack::Attack
-
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
