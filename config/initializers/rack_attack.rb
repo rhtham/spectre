e19289b48365f8162cb211fd27c4c00fc6349ba9
Rick Tham <rtham@onenorth.com>
2019-08-23 11:21:49 -0500

    more ip formating issues

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -1,10 +1,10 @@
 if ENV["IP_WHITELIST"].present?
 
-  allowed = Set.new[ENV["IP_WHITELIST"]]
+  allowed = %w[ ENV["IP_WHITELIST"] ].to_set
 
   Rack::Attack.blocklist("block all access") do |req|
     # Requests are blocked if the return value is truthy
     !allowed.include? req.ip
   end
-  
+
 end
