0274e1e1deed0827cac91c1c1c26c961a5dc54fd
Rick Tham <rhtham@gmail.com>
2022-02-11 12:14:22 -0600

    test

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -2,6 +2,10 @@ if ENV["IP_WHITELIST"].present?
 
   allowed = ENV["IP_WHITELIST"]
 
+  if ENV["GP_OFFICE_CIDR"].present?
+    Rack::Attack.safelist_ip(ENV["GP_OFFICE_CIDR"])
+  end
+
   Rack::Attack.blocklist("block all access") do |req|
     # Requests are blocked if the return value is truthy
     !allowed.include? req.ip
