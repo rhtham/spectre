c7b336ca728c9a4af4461733437ebcd02d602a88
Rick Tham <rhtham@gmail.com>
2022-02-11 12:00:00 -0600

    test2

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -1,6 +1,8 @@
 if ENV["IP_WHITELIST"].present?
 
-  allowed = ENV["IP_WHITELIST"]
+  allowed = Set.new[ENV["IP_WHITELIST"]]
+ 
+  allowed.each { |ip_address|Rack::Attack.safelist_ip(ip_address) }
 
   Rack::Attack.blocklist("block all access") do |request|
     # Requests are blocked if the return value is truthy
