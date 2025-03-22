1f1e616fd9fa4ebb09bc3dc77b0a5f1a5de3a757
Tham, Rick <rtham@onenorth.com>
2025-03-21 20:02:41 -0700

    npm issue

--- config/initializers/jsbundling.rb
+++ config/initializers/jsbundling.rb
@@ -1,4 +1,10 @@
-# Set the preferred JavaScript package manager
-if Rails.env.development? || Rails.env.test?
+# Set the JavaScript package manager to use
+if File.exist?("#{Rails.root}/yarn.lock")
+  ENV["JAVASCRIPT_PACKAGE_MANAGER"] ||= "yarn"
+elsif File.exist?("#{Rails.root}/package-lock.json")
+  ENV["JAVASCRIPT_PACKAGE_MANAGER"] ||= "npm"
+elsif File.exist?("#{Rails.root}/bun.lockb")
+  ENV["JAVASCRIPT_PACKAGE_MANAGER"] ||= "bun"
+else
   ENV["JAVASCRIPT_PACKAGE_MANAGER"] ||= "npm"
 end
